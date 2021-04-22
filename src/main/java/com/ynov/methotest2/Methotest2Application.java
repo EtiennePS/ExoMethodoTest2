1b51f71eb9b4bc50289e72e83775e26ce73ebf5d
EtiennePS <etienne.picherit@gmail.com>
2021-04-22 13:28:25 +0200

    Initialisation du projet spring boot

---
+++ src/main/java/com/ynov/methotest2/Methotest2Application.java
@@ -0,0 +1,13 @@
+package com.ynov.methotest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Methotest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Methotest2Application.class, args);
+	}
+
+}
